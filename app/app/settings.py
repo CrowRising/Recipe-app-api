46bf5f535f017a11c920d8624a2669ea744664b1
Crow Rising <miscbyrd@gmail.com>
2023-09-27 16:47:04 -0600

    debug configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,18 +16,22 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!pfjuwmtikn@fmy*x5w36t1_9opd=$3b#om98sl)zoovy*98p@'
+SECRET_KEY = os.environ.get('SECRET_KET', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED _HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
